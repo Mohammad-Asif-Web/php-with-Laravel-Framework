f513356e6114af674033bd9c564260aa464c7157
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2022-04-06 22:54:46 +0600

    project successfully done

--- class-18-BMI calculator/app.js
+++ class-18-BMI calculator/app.js
@@ -6,9 +6,7 @@ range.addEventListener("input", ()=>{
     val.innerText = range.value
 })
 
-
-
-// increment/Decrement value by button click
+// Age calculation
 let val2 = document.getElementById("inc")
 const decBtn = document.getElementById("decBtn")
 const incBtn = document.getElementById("incBtn")
@@ -24,9 +22,73 @@ decBtn.addEventListener("click", ()=>{
 
 })
 
+// weight calculation
+let wValue = document.getElementById("wValue")
+const wminBtn = document.getElementById("wminBtn")
+const wMaxBtn = document.getElementById("wMaxBtn")
 
+var j = 0;
+wMaxBtn.addEventListener("click", ()=>{
+    wValue.innerText = ++j;
+})
+wminBtn.addEventListener("click", ()=>{
+    if(j > 0){
+        wValue.innerText = --j;
+      }
 
+})
 
+// project one code
+let btn = document.getElementById('calc');
+
+btn.addEventListener("click", ()=>{
+    let bmi;
+    let weight = parseInt(document.getElementById("wValue").innerText)
+    // let ft = parseInt(document.getElementById("ft").value)
+    // let inch = parseInt(document.getElementById("inch").value)
+
+    // let meter = (((ft * 12) + inch) * .0254);
+    // meter = Math.pow(meter, 2);
+    let meter = parseInt(document.getElementById("val").innerText)
+    // meter = Math.pow(meter, 2);
+    
+
+    bmi = weight / (meter * .0254)
+    console.log(bmi)
+    console.log(weight);
+    console.log(meter)
+
+    // const div = document.querySelector(".wrapper");
+    const p = document.getElementById("value")
+    const span = document.getElementById("result")
+
+    p.innerText = bmi
+    if(bmi < 18.5){
+        p.innerHTML = bmi;
+        span.innerText = "Underweight"
+    }
+    else if(bmi > 18.5 && bmi < 25){
+        p.innerHTML = bmi;
+        span.innerText = "Normal Weight"
+        span.style = "color: green"
+        p.style = "color: green"
+    }
+    else if(bmi > 25 && bmi < 30){
+        p.innerHTML = bmi;
+        span.innerText = "Overweight"
+        span.style = "color: blue"
+        p.style = "color: blue"
+    }
+    else if(bmi > 30){
+        p.innerHTML = bmi;
+        span.innerText = "obese"
+        span.style = "color: red"
+        p.style = "color: red"
+    }
+    else{
+        alert("wrong value enter")
+    }
+})
 
 
 
