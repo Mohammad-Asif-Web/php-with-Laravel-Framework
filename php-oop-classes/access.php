d794ae979f10c84b2b4f7e6659cfe3c90f456593
Mohammad-Asif-Web <alexasifhhossain@gmail.com>
2022-05-03 01:51:34 +0600

    new object files created and working..

---
+++ php-oop-classes/access.php
@@ -0,0 +1,23 @@
+<?php
+include 'something.php';
+
+// object is accessed by -> sign
+// array is accessed by => sign
+
+$add = new Something;
+echo $add -> add(10, 30);
+echo '<br>';
+
+$sub = new Something;
+echo $sub -> sub(30, 10);
+echo '<br>';
+
+
+
+
+
+
+
+
+
+
---
+++ php-oop-classes/something.php
@@ -0,0 +1,34 @@
+<?php
+
+class Something{
+    function add($num1, $num2){
+        $res = $num1 + $num2;
+        return "your addition is ${res}";
+    }
+
+    function sub($num1, $num2){
+        $res = $num1 + $num2;
+        return "your substration is ${res}";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
